3aac312be30236207a0047087c68efa4a90c727e
Aimrane Essakhi <aimrane.essakhi@gmail.com>
2021-03-10 23:37:46 +0100

    update

--- EasyHome/EasyHome/lib/app/sing_in/background.dart
+++ EasyHome/EasyHome/lib/app/sing_in/background.dart
@@ -15,9 +15,9 @@ class Background extends StatelessWidget {
         children: [
           Positioned(
             top: 50,
-            left: 30,
+            right: 30,
             child: Image.asset("assets/images/EASYHOME.png",
-                width: size.width * 0.25),
+                width: size.width * 0.20),
           ),
           Positioned(
             top: 0,
